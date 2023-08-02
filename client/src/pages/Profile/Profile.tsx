a5546400b6a928d6759b36ed0e2a8e076f4a4124
seanmckee <seanmingmckee@gmail.com>
2023-08-01 21:52:27 -0700

    front end form and handlers created for profile

--- client/src/pages/Profile/Profile.tsx
+++ client/src/pages/Profile/Profile.tsx
@@ -16,6 +16,11 @@ type User = {
   workouts: any[];
 };
 
+type Height = {
+  feet: number;
+  inches: number;
+};
+
 const initializeUser = {
   username: "",
   email: "",
@@ -34,8 +39,30 @@ const Profile = () => {
   const [cookies] = useCookies(["access_token"]);
   const userID = window.localStorage.getItem("userID");
   const [user, setUser] = useState<User>(initializeUser);
+  const [height, setHeight] = useState<Height>({ feet: 0, inches: 0 });
   // const [username, setUsername] = useState("");
   // const [weight, setWeight] = useState(0);
+
+  // change any number based form input value change
+  const handleNumberedChange = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    id: keyof User
+  ) => {
+    const value = e.target.value;
+    const newValue =
+      value === "" ? 0 : isNaN(Number(value)) ? user?.[id] : Number(value);
+    setUser({ ...user, [id]: newValue });
+  };
+
+  const handleHeightChange = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    id: keyof Height
+  ) => {
+    const value = e.target.value;
+    const newValue =
+      value === "" ? 0 : isNaN(Number(value)) ? height?.[id] : Number(value);
+    setHeight({ ...height, [id]: newValue });
+  };
 
   const fetchProfile = async () => {
     try {
@@ -44,7 +71,6 @@ const Profile = () => {
         { headers: { authorization: cookies.access_token } }
       );
       setUser(response.data);
-      console.log(response.data);
     } catch (error) {
       console.error(error);
     }
@@ -54,6 +80,12 @@ const Profile = () => {
     fetchProfile();
   }, []);
 
+  // keeps track of total height in inches
+  useEffect(() => {
+    const heightInInches = height.feet * 12 + height.inches;
+    setUser({ ...user, height: heightInInches });
+  }, [height]);
+
   return (
     <div className="pt-[75px] p-5">
       <h1>View or Edit Information Here</h1>
@@ -61,20 +93,76 @@ const Profile = () => {
         <h1 className="text-xl mr-4 mt-2">Username: </h1>
         <input
           type="text"
-          placeholder={"Type here"}
+          placeholder={"Enter Username"}
           className="input input-bordered w-full max-w-xs"
           value={user?.username}
           onChange={(e) => setUser({ ...user, username: e.target.value })}
         />
       </div>
       <div className="flex">
-        <h1 className="text-xl mr-4 mt-2">Weight(lbs): </h1>
+        <h1 className="text-xl mr-4 mt-2">Weight: </h1>
         <input
-          type="number"
-          placeholder={"Type here"}
-          className="input input-bordered w-full max-w-xs [-moz-appearance:_textfield] [&::-webkit-inner-spin-button]:m-0 [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:m-0 [&::-webkit-outer-spin-button]:appearance-none"
-          value={String(user?.weight)}
-          onChange={(e) => setUser({ ...user, weight: Number(e.target.value) })}
+          type="text"
+          inputMode="numeric"
+          placeholder={"Enter Weight (lbs)"}
+          className="input input-bordered w-full max-w-xs"
+          value={user?.weight === 0 ? "" : String(user?.weight)}
+          onChange={(e) => handleNumberedChange(e, "weight")}
+        />
+      </div>
+      <div className="flex">
+        <h1 className="text-xl mr-4 mt-2">Age: </h1>
+        <input
+          type="text"
+          inputMode="numeric"
+          placeholder={"Enter Age"}
+          className="input input-bordered w-full max-w-xs"
+          value={user?.age === 0 ? "" : String(user?.age)}
+          onChange={(e) => handleNumberedChange(e, "age")}
+        />
+      </div>
+
+      <div className="flex">
+        <h1 className="text-xl mr-4 mt-2">Calorie Goal: </h1>
+        <input
+          type="text"
+          inputMode="numeric"
+          placeholder={"Enter Calorie Goal"}
+          className="input input-bordered w-full max-w-xs"
+          value={user?.goalCalories === 0 ? "" : String(user?.goalCalories)}
+          onChange={(e) => handleNumberedChange(e, "goalCalories")}
+        />
+      </div>
+
+      <div className="flex">
+        <h1 className="text-xl mr-4 mt-2">Weight Goal: </h1>
+        <input
+          type="text"
+          inputMode="numeric"
+          placeholder={"Enter Weight Goal (lbs)"}
+          className="input input-bordered w-full max-w-xs"
+          value={user?.goalWeight === 0 ? "" : String(user?.goalWeight)}
+          onChange={(e) => handleNumberedChange(e, "goalWeight")}
+        />
+      </div>
+      <div className="flex">
+        <h1 className="text-xl mr-4 mt-2">Height ([ft]/[in]): </h1>
+        <input
+          type="text"
+          inputMode="numeric"
+          placeholder={"Enter Height (ft)"}
+          className="input input-bordered w-full max-w-xs"
+          value={height.feet === 0 ? "" : String(height.feet)}
+          onChange={(e) => handleHeightChange(e, "feet")}
+        />
+
+        <input
+          type="text"
+          inputMode="numeric"
+          placeholder={"Enter Height (in)"}
+          className="input input-bordered w-full max-w-xs"
+          value={height.inches === 0 ? "" : String(height.inches)}
+          onChange={(e) => handleHeightChange(e, "inches")}
         />
       </div>
 
