ade786123e83d094cdf339c6ece880040142faef
seanmckee <seanmingmckee@gmail.com>
2023-10-05 19:51:00 -0700

    get started button navigates based on auth status

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,7 +1,17 @@
+import { useNavigate } from "react-router-dom";
 import lifter from "../../assets/lifter.png";
 import { BsFillArrowRightCircleFill } from "react-icons/bs";
 
 const Home = () => {
+  const navigate = useNavigate();
+
+  const onClick = () => {
+    if (window.localStorage.getItem("userID")) {
+      navigate("/schedule");
+    } else {
+      navigate("/auth");
+    }
+  };
   return (
     <div>
       <div className="grid lg:grid-cols-2 bg-base-200 h-screen">
@@ -9,7 +19,7 @@ const Home = () => {
           <h1 className="font-bold  mt-0 text-pink-500 lg:text-9xl">Fitness</h1>
           <h1 className="font-bold  mt-5 text-yellow-500">Should Be</h1>
           <h1 className="font-bold mt-5 text-blue-500">Simple</h1>
-          <button className="btn btn-outline btn-secondary">
+          <button onClick={onClick} className="btn btn-outline btn-secondary">
             Get Started <BsFillArrowRightCircleFill size={18} />
           </button>
         </div>
